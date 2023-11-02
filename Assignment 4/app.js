031e21670e798a5d520746cc55c830b1df9a4110
akram <akramhany65@gmail.com>
2023-11-02 17:32:28 +0200

    handle routing in the application and make sure that its working

--- Assignment 4/app.js
+++ Assignment 4/app.js
@@ -2,8 +2,14 @@ const express = require('express');
 
 const app = express();
 
-app.use((req, res, next) => {
-    console.log(req.url);
-})
+const adminRoutes = require('./routes/admin.js');
+const usersRoutes = require('./routes/users.js');
+
+app.set('view engine', 'ejs')
+app.set('views', 'views');
+
+app.use('/admin', adminRoutes.router);
+
+app.use('/users', usersRoutes);
 
 app.listen(3000);
